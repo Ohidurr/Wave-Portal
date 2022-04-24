a42d48503e427939cef86f5081cc6374b94cff9c
ohidurr <ohidurrahman96@gmail.com>
2022-04-24 15:29:40 -0400

    configure wallet auth and button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,88 @@ import React, { useEffect, useState } from "react"
 import { ethers } from ethers; 
 import './App.css';
 
-const App= () => {
-  const [currentAccount, setCurrentAccount] = useState("")
- 
-}
+const App = () => {
+  /*
+  * Just a state variable we use to store our user's public wallet.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
 
-export default App;
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+      
+      /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const connectWallet = async () =>{
+    try{
+      const { ethereum } = window;
+
+      if(!ethereum){
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({method: "eth_requestAccounts"});
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch(error){
+      console.log(error)
+    }
+  }
+  /*
+  * This runs our function when the page loads.
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there!
+        </div>
+
+        <div className="bio">
+          I am Ohidur. Nice to meet you.
+        </div>
+
+        <button className="waveButton" onClick={null}>
+          Wave at Me
+        </button>
+
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+export default App
